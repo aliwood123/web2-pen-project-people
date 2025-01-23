e0083db0b23b1cdc01d081cb1458ba68f3495875
Alicia Woodard <awoodard1@ewu.edu>
2025-01-23 20:01:20 +0000

    add js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-// website loaded?
-console.log(Okay);
+let confetti = new Confetti("confetti");
+confetti.destroyTarget(false);
+confetti.setSize(10);
+confetti.setCount(100);
+confetti.setPower(100);
